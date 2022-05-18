68a0bfdcdb5adda5dc50ec2246c15cb794930e4a
willson97 <104342529+G00F7@users.noreply.github.com>
2022-05-18 12:13:39 -0400

    Add files via upload

---
+++ pregunta4/pregunta4.R
@@ -0,0 +1,6 @@
+baraja_df <- data.frame(
+  "simbolo" = c(rep("espada",13),rep("rombo",13),rep("trebol",13),rep("corazon",13)),
+  "color" = c(rep("negro",13),rep("rojo",13),rep("negro",13),rep("rojo",13)),
+  "numero" = c(rep(1:13,4))
+)
+baraja_df
